26503f7a2bb6d82c886b63ef85181376babff4c2
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 16:24:05 +0000

    Added answers

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import numpy as np
 
 def getBinary( N ) : 
   # Your code goes here
-  
+  bina = np.zeros(6)
+  for i in range(6) :
+     pp = 2**(5-i)
+     bina[i] = np.floor( N / pp )
+     N = N - bina[i]*pp
+  return bina
 
 for i in range(16) : 
   print("The binary representation of", i, "is", getBinary(i) )
